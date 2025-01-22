109ff38d7fff44f0b670a2a2fbae6719aa3d6857
Jonathan Perkins <jonperk318@gmail.com>
2025-01-21 19:20:01 -0500

    Added TCP Listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connected!");
+    }
+
 }
