190a80b53fb280b5a1c98a0d2b157ee6fb468c97
Jonathan Perkins <jonperk318@gmail.com>
2025-01-29 11:01:22 -0500

    Server now serves HTML

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpStream},
 };
@@ -15,7 +16,12 @@ pub fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {http_request:#?}");
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("public/index.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 
