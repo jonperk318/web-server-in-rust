eb5fc45c6be3b19834019ab1e5dc21345056fea6
Jonathan Perkins <jonperk318@gmail.com>
2025-02-03 10:02:06 -0500

    Added 404 for all other cases

--- src/connection.rs
+++ src/connection.rs
@@ -8,21 +8,32 @@ use std::{
 pub fn handle_connection(mut stream: TcpStream) {
 
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {http_request:#?}");
+    if request_line == "GET / HTTP/1.1" { // 200
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("public/index.html").unwrap();
-    let length = contents.len();
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("public/index.html").unwrap();
+        let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+
+    } else { // 404
+
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("public/404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+
+    }
 
 }
