85ec0af13b86bcf2a56844f93e22b7363a8f6738
Jonathan Perkins <jonperk318@gmail.com>
2025-02-04 12:01:44 -0500

    Improved handle_connection and added page

--- src/connection.rs
+++ src/connection.rs
@@ -10,30 +10,20 @@ pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" { // 200
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "public/index.html")
+    } else if request_line == "GET /page HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "public/page.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
 
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("public/index.html").unwrap();
-        let length = contents.len();
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
-
-    } else { // 404
-
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("public/404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 
 }
