4c2707ae95a9fa9658ee99dbbf47785ed6d3c03a
Jonathan Perkins <jonperk318@gmail.com>
2025-03-04 08:59:17 -0500

    Modularized multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::net::TcpListener;
 
-use web_server_in_rust::ThreadPool;
-
 mod connection;
+mod multithreading;
 
 
 fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = multithreading::ThreadPool::new(4);
 
     for stream in listener.incoming().take(100) {
 
